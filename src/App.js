63d57f1f134ad9ea3774e4caab4d77b7736b6e04
yacaresistemas <yacaresistemas@gmail.com>
2022-07-24 15:10:54 -0300

    avances

--- src/App.js
+++ src/App.js
@@ -7,8 +7,178 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { Routes, Route, Outlet, NavLink, Link } from 'react-router-dom';
+import {LinkContainer} from 'react-router-bootstrap'
 
 
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  const style2 = {
+    color: "white",
+  };
+
+
+  return (
+    <div className="App">
+      <Navbar expand="lg" style={{backgroundColor: "#0F6972"}}>
+        <Container>
+          <Navbar.Brand href="#inicio" style={{color: "#FFFFFF", fontSize: 28}}>
+            BuilderX
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="sitio-nav" />
+          <Navbar.Collapse id="sitio-nav" className="justify-content-center">
+            <Nav>
+              <LinkContainer to="/">
+                  <Nav.Link>Inicio</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/temas">
+                  <Nav.Link>Temas</Nav.Link>
+              </LinkContainer>
+
+             {/*  <Nav.Link href="/" style={style2}>Inicio</Nav.Link>
+              <Nav.Link href="/temas" style={style2}>Temas</Nav.Link>             
+              <Nav.Link href="#contacto" style={style2}>Contacto</Nav.Link>      */}       
+            </Nav>
+          </Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="#ingresar">
+              <Button style={{backgroundColor: 'transparent', color: "white", borderColor: "#FFFFFF"}}>
+                <FontAwesomeIcon icon={faUser} /> Ingresar
+              </Button>
+            </Nav.Link>
+            <Nav.Link href="#registrarse">
+              <Button style={{backgroundColor: "#D88317", color: "white", borderColor: "#D88317"}}>
+                <FontAwesomeIcon icon={faUserPlus} /> Registrarse
+              </Button>
+            </Nav.Link>       
+          </Nav>
+        </Container>
+      </Navbar>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="temas" element={<Temas />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Inicio() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/temas">Temas</Link>
+      </nav>
+    </>
+  );
+}
+
+function Temas() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          That feels like an existential question, don't you
+          think?
+        </p>
+      </main>
+      <nav>
+        <Link to="/">Inicio</Link>
+      </nav>
+    </>
+  );
+}
+
+export default App;
+
+/*
+const Layout = () => {
+  const style = ({ isActive }) => ({
+    fontWeight: isActive ? 'bold' : 'normal',
+  });
+
+  const style2 = {
+    color: "white",
+  };
+
+  return (
+    <>      
+      <Navbar expand="lg" style={{backgroundColor: "#0F6972"}}>
+        <Container>
+          <Navbar.Brand href="#inicio" style={{color: "#FFFFFF", fontSize: 28}}>
+            BuilderX
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="sitio-nav" />
+          <Navbar.Collapse id="sitio-nav" className="justify-content-center">
+            <Nav>
+              <Nav.Link href="/" style={style2}>Inicio</Nav.Link>
+              <Nav.Link href="/temas" style={style2}>Temas</Nav.Link>             
+              <Nav.Link href="#contacto" style={style2}>Contacto</Nav.Link>            
+            </Nav>
+          </Navbar.Collapse>
+          <Nav>
+            <Nav.Link href="#ingresar">
+              <Button style={{backgroundColor: 'transparent', color: "white", borderColor: "#FFFFFF"}}>
+                <FontAwesomeIcon icon={faUser} /> Ingresar
+              </Button>
+            </Nav.Link>
+            <Nav.Link href="#registrarse">
+              <Button style={{backgroundColor: "#D88317", color: "white", borderColor: "#D88317"}}>
+                <FontAwesomeIcon icon={faUserPlus} /> Registrarse
+              </Button>
+            </Nav.Link>       
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <main style={{ padding: '1rem 0' }}>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+function Inicio() {
+  return (
+    <Container>
+      <Row>
+        <Col>En Inicio</Col>
+      </Row>
+    </Container>
+  );
+}
+
+const Temas = () => {
+  return (
+    <>
+      <h2>en Temas</h2>
+    </>
+  );
+};
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Inicio />} />
+        <Route path="" element={<Inicio />} />
+        <Route path="temas" element={<Temas />} />
+        <Route path="*" element={<p>There's nothing here: 404!</p>} />
+      </Route>
+    </Routes>
+  );
+};
+
+*/
+
+/*
 function App() {
   const style = {
     color: "white",
@@ -44,5 +214,6 @@ function App() {
     </Navbar>
   );
 }
+*/
 
-export default App;
+//export default App;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from "react-router-dom";
+import Header from './components/Header.js'
+import Service from './components/service.js'
+import About from './components/about.js'
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
+
+
+/*
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
